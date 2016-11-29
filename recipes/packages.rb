4e96ce34e021ef9388346127c651868980e88d5e
Matthew Davis <mattd@copiousinc.com>
2016-11-29 12:05:32 -0800

    Include epel-release package for centos.

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -1,7 +1,13 @@
+case node['platform']
+when 'centos'
+    include_recipe 'cop_base::yum'
+end
+
 generic_packages = node.default['packages']
 
 generic_packages.each do |pkg|
     package pkg do
         action :install
+        options '--enablerepo=epel' if node['platform'] == 'centos'
     end
 end
