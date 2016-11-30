c1ea974b5bdcf4463d4bd170e37b31293b906213
Matthew Davis <mattd@copiousinc.com>
2016-11-30 11:31:53 -0800

    Do not set priority of attribute.

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -1,6 +1,4 @@
-generic_packages = node.default['packages']
-
-generic_packages.each do |pkg|
+node['packages'].each do |pkg|
     package pkg do
         action :install
         options '--enablerepo=epel' if node['platform_family'] == 'rhel'
