02b814b3c2f99ceec2498d154c8f9a7bbc74a6fb
Colby Olson <me@colbyolson.com>
2016-11-30 16:50:19 -0800

    bump to 0.0.7

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email    'engineering@copiousinc.com'
 license             'MIT'
 description         'Base OS packages.'
 long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version             '0.0.6'
+version             '0.0.7'
 source_url          'https://github.com/copious-cookbooks/base'
 issues_url          'https://github.com/copious-cookbooks/base/issues'
 
