7f5a8ff98fde3a9231e37c75d10091714409c439
Colby Olson <me@colbyolson.com>
2016-12-12 15:07:18 -0800

    allow for epel name changes

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,6 +8,9 @@ case node['platform_family']
 when 'debian'
     default['packages']['install'] += %w(autoconf binutils-doc bison build-essential flex gettext ncurses-dev)
 when 'fedora', 'rhel'
+    default['epel']['package_name'] = 'epel-release'
+    default['epel']['repo_name']    = 'epel'
+
     default['packages']['install'] += %w(autoconf bison flex gcc gcc-c++ gettext kernel-devel make m4 ncurses-devel patch)
     default['packages']['install'] += %w(gcc44 gcc44-c++) if node['platform_version'].to_i < 6
 end
