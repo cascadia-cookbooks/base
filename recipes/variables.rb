3bc44441c37e282785a99f239190c4dd64a59d43
Colby Olson <me@colbyolson.com>
2017-01-04 13:30:51 -0800

    dont update if attributes are empty

--- recipes/variables.rb
+++ recipes/variables.rb
@@ -2,8 +2,10 @@ file '/etc/environment' do
     action :touch
 end
 
-node['environment_vars'].each do |variable, value|
-    execute variable do
-        command "echo '#{variable}=#{value}' >> /etc/environment"
+unless node['environment_vars'].nil? || node['environment_vars'].empty?
+    node['environment_vars'].each do |variable, value|
+        execute variable do
+            command "echo '#{variable}=#{value}' >> /etc/environment"
+        end
     end
 end
