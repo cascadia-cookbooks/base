59e856a1a12f4595c8ce57f59e1a284d0b3d2c69
Colby Olson <me@colbyolson.com>
2017-01-04 13:32:16 -0800

    add note

--- recipes/variables.rb
+++ recipes/variables.rb
@@ -9,6 +9,7 @@ unless node['environment_vars'].nil? || node['environment_vars'].empty?
     node['environment_vars'].each do |variable, value|
         execute variable do
             command "echo '#{variable}=#{value}' >> /etc/environment"
+            # NOTE: this grep might be aggresive
             not_if  "grep #{variable} /etc/environment"
         end
     end
