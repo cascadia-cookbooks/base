74d2d821c6245eb31f4b5cbd00b5f825b0c81913
Colby Olson <me@colbyolson.com>
2017-01-04 13:49:23 -0800

    include some tests

---
+++ test/integration/default/serverspec/default_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'base::default' do
+  describe package('ntp') do
+    it { should be_installed }
+  end
+
+  describe package('git') do
+    it { should be_installed }
+  end
+
+  describe file('/etc/environment') do
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_mode '644' }
+    it { should contain 'testing=true' }
+  end
+end
